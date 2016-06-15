4e3cd09757a647fe93627fb1591662100ace3381
Francis Giraldeau <francis.giraldeau@gmail.com>
2016-06-15 17:31:36 -0400

    Reboot VM once the program is completed
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- debug.sh
+++ debug.sh
@@ -20,8 +20,10 @@ cp a.out mnt/opt
 sudo umount linux.img
 
 qemu-system-x86_64 \
+    -enable-kvm \
     -hda linux.img \
-    -serial stdio \
+    -no-reboot \
+    -nographic \
     -kernel ../linux/arch/x86/boot/bzImage \
     -virtfs local,path=/home/francis,mount_tag=host0,security_model=passthrough,id=host0 \
-    -append "root=/dev/sda init=/opt/a.out console=ttyS0"
+    -append "root=/dev/sda init=/opt/a.out console=ttyS0 panic=1"
