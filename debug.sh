32b634c9e269f67117b5b6a01d7241adf355e501
Jean-Alexandre Barszcz <jalex_b@hotmail.com>
2016-06-16 17:35:03 -0400

    removed hardcoded paths from debug script

--- debug.sh
+++ debug.sh
@@ -1,4 +1,29 @@
-#!/bin/sh -x
+#!/usr/bin/env bash
+
+function usage {
+    echo "Usage : $0 kernel_image_path [ virtfs_path ]"
+}
+
+if [[ "$#" -lt 1 ]]; then
+    usage
+    echo "Looking for images :"
+    RES=$(find .. -name bzImage -type f 2> /dev/null)
+    if [[ -z $RES ]]; then
+	echo "    none found"
+    else
+	echo $RES
+    fi
+    exit 1
+elif [[ "$#" -gt 2 ]]; then
+    usage
+    exit 1
+elif [[ "$#" -eq 2 ]]; then
+    QEMU_VIRTFS_ARGS="-virtfs local,path=$2,mount_tag=host0,security_model=passthrough,id=host0"
+fi
+
+KERNEL_IMAGE_PATH=$1
+
+set -x
 
 cat <<EOF > main.c 
 #include <stdlib.h>
@@ -20,10 +45,13 @@ cp a.out mnt/opt
 sudo umount linux.img
 
 qemu-system-x86_64 \
-    -enable-kvm \
     -hda linux.img \
     -no-reboot \
     -nographic \
-    -kernel ../linux/arch/x86/boot/bzImage \
-    -virtfs local,path=/home/francis,mount_tag=host0,security_model=passthrough,id=host0 \
-    -append "root=/dev/sda init=/opt/a.out console=ttyS0 panic=1"
+    -kernel $KERNEL_IMAGE_PATH \
+    $QEMU_VIRTFS_ARGS \
+    -append "root=/dev/sda init=/opt/a.out console=ttyS0 panic=1" \
+    -s
+
+# starts gdb server on localhost:1234
+# hook from client with: target remote localhost:1234
