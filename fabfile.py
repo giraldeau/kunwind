ee4efecabb0b4f649405547c10284162c2a36366
Francis Giraldeau <francis.giraldeau@gmail.com>
2016-09-30 11:27:48 -0400

    Cleanup fabfile
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- fabfile.py
+++ fabfile.py
@@ -1,53 +1,117 @@
 #!/usr/bin/env python
 
-from fabric.api import task, run, sudo, local, cd, lcd, put, reboot
 import os
+import glob
+import yaml
+import re
 
-module_name = "kunwind-debug"
+from fabric.api import task, run, sudo, local, cd, lcd, put, reboot, execute, env, settings, hide
+from fabric.contrib.project import rsync_project
+from fabric.contrib.files import exists
+from fabric.context_managers import warn_only
 
-@task
-def setup(linux_dir="../linux", makeopts=""):
-    # TODO generate config from oldconfig
-    # install kernel
-    with lcd(linux_dir):
-        kvern = local("make kernelversion", capture=True)
-        kver = "{}-test".format(kvern)
-        local("make {}".format(makeopts))
-        with cd("/boot"):
-            put("arch/x86/boot/bzImage", "vmlinuz-{}".format(kver), use_sudo=True)
-            put("System.map", "System.map-{}".format(kver), use_sudo=True)
-            put(".config", "config-{}".format(kver), use_sudo=True)
+top_srcdir = os.path.abspath(".")
+env_file = os.path.join(top_srcdir, "env.yaml")
+if not os.path.exists(env_file):
+    raise Exception("configure your environment in env.yaml file (see env.yaml.example)")
 
-    # install modules
-    with lcd(linux_dir):
-        local("make {} modules_install INSTALL_MOD_PATH=temp/ KDIR={}".format(makeopts, linux_dir))
-        dest_base = "/lib/modules"
-        dest = os.path.join(dest_base, kver)
-        src_dir = os.path.join("temp/lib/modules/", kvern)
-        sudo("rm -rf {}".format(dest), warn_only=True)
-        sudo("mkdir -p {}".format(dest_base))
-        put(src_dir, dest_base, use_sudo=True)
-        sudo("mv {}/{} {}".format(dest_base, kvern, dest))
+config = {}
+with open(env_file, "r") as f:
+    config = yaml.load(f)
 
-    sudo("update-initramfs -c -k {}".format(kver))
-    sudo("update-grub2")
-    reboot()
+env.hosts = config["host"]
+env.user = config["user"]
 
 @task
-def runtest(linux_dir="../linux", makeopts=""):
-    # Make + copy module
-    local("make {} KDIR={}".format(makeopts, linux_dir))
-    put("{}.ko".format(module_name), "")
+def check_config():
+    if not os.path.exists(config["linux_dir"]):
+        raise Exception("error: linux sources not found, create a symlink to your sources with: ln -sf ../linux")
 
-    # Make userspace test program (on guest since library versions
-    # might differ)
-    run("rm -r kunwind/", warn_only=True)
-    run("mkdir -p ./kunwind")
-    put("./include", "./kunwind/") # TODO other syncing options? delta-sync?
-    put("./test", "./kunwind/")
-    run("make {} -C ./kunwind/test".format(makeopts))
+@task
+def build():
+    local("make KDIR={linux_dir} CONFIG_KUNWIND_DEBUG=m".format(**config))
 
-    # Run everything
-    sudo("insmod {}.ko".format(module_name))
-    run("./kunwind/test/test")
-    sudo("rmmod {}".format(module_name))
+@task
+def clean():
+    local("make KDIR={linux_dir} clean".format(**config))
+
+@task
+def rebuild():
+    execute(clean)
+    execute(build)
+
+@task
+def hello():
+    run("date")
+
+@task
+def push():
+    mods = glob.glob("*.ko")
+    for mod in mods:
+        put(mod, "")
+
+@task
+def load():
+    mods = glob.glob("*.ko")
+    for mod in mods:
+        sudo("insmod {}".format(mod))
+
+@task
+def unload():
+    r = re.compile("(?P<mod>[\w-]+)\.ko")
+    mods = glob.glob("*.ko")
+    for mod in mods:
+        name = r.match(mod).group("mod")
+        with settings(warn_only=True):
+            with hide('output', 'warnings'):
+                sudo("rmmod {}".format(name))
+
+@task
+def reload():
+    execute(unload)
+    execute(load)
+
+@task
+def test():
+    rsync_project("", "libkunwind")
+    rsync_project("", "include")
+    with cd("libkunwind"):
+        if not exists("configure"):
+            run("./bootstrap")
+            run("./configure")
+        run("make check")
+
+@task
+def check():
+    execute(build)
+    execute(reload)
+    execute(test)
+
+@task
+def setup():
+    sudo("apt-get install -q -y rsync build-essential autoconf libtool")
+
+# TODO: fix the kernel compilation
+#     with lcd(linux_dir):
+#         kvern = local("make kernelversion", capture=True)
+#         kver = "{}-test".format(kvern)
+#         local("make {}".format(makeopts))
+#         with cd("/boot"):
+#             put("arch/x86/boot/bzImage", "vmlinuz-{}".format(kver), use_sudo=True)
+#             put("System.map", "System.map-{}".format(kver), use_sudo=True)
+#             put(".config", "config-{}".format(kver), use_sudo=True)
+# 
+#     # install modules
+#     with lcd(linux_dir):
+#         local("make {} modules_install INSTALL_MOD_PATH=temp/ KDIR={}".format(makeopts, linux_dir))
+#         dest_base = "/lib/modules"
+#         dest = os.path.join(dest_base, kver)
+#         src_dir = os.path.join("temp/lib/modules/", kvern)
+#         sudo("rm -rf {}".format(dest), warn_only=True)
+#         sudo("mkdir -p {}".format(dest_base))
+#         put(src_dir, dest_base, use_sudo=True)
+#         sudo("mv {}/{} {}".format(dest_base, kvern, dest))
+# 
+#     sudo("update-initramfs -c -k {}".format(kver))
+#     sudo("update-grub2")
+#     reboot()
